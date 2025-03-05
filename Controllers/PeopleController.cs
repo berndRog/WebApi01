57f37f52bd0e4de89c97e67a8ddb272ef3cb58aa
Bernd-Uwe Rogalla <b-u.rogalla@ostfalia.de>
2025-03-05 17:20:31 +0100

    Rename PersonController to PeopleController for consistency.

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -7,7 +7,7 @@ namespace WebApi.Controllers;
 [Route("carshop")]
 //[Consumes("application/json")] default
 //[Produces("application/json")] default
-public class PersonController(
+public class PeopleController(
    IPeopleRepository peopleRepository,
    IDataContext dataContext
    //ILogger<PersonController> logger
